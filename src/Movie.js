604a7f040ab2ad38ae7fbf74315a1a4ed3aebfe8
y-solb <ysolb99@gmail.com>
2021-09-25 21:19:11 +0900

    style: movie 틀 디자인 수정

--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,7 @@ function Movie({ year, title, summary, poster, genres }) {
   return (
     <li className="movie">
       <img className="movie__poster" src={poster} alt={title} title={title} />
-      {/* <div className="movie__data">
+      <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
         <ul className="genres">
@@ -17,7 +17,7 @@ function Movie({ year, title, summary, poster, genres }) {
           ))}
         </ul>
         <p className="movie__summary">{summary.slice(0, 180)}...</p>
-      </div> */}
+      </div>
     </li>
   );
 }
